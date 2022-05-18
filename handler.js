737516e5c57baae34e344f35047dfa54b133a584
Ivan Andrade <ivanandrade611@gmail.com>
2022-05-18 17:25:08 -0500

    Se agrego logica de la solucion

--- handler.js
+++ handler.js
@@ -1,15 +1,40 @@
 'use strict';
 
 module.exports.solution = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Creando la infra con Serverless!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+    let gruposDeAmigos = event.groups;
+    let nPersonas = 0;
+    let sizesBus = '';
+    let capacidad = 0;
+
+    for(let grupo of gruposDeAmigos) {
+        if (grupo !== ',' && !isNaN(grupo)){
+            grupo = parseInt(grupo);
+            nPersonas += grupo;
+            capacidad = grupo > capacidad ? grupo : capacidad;
+        }
+    }
+   
+    while (capacidad <= nPersonas) {
+        if(( nPersonas % capacidad ) == 0){
+            sizesBus += `${capacidad},`
+        }
+        capacidad++;
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                sizes: sizesBus.slice(0, -1)
+            },
+            null,
+            2
+        ),
+    };
 };
+
+// const entrada = { "groups": "1,2,1,1,1,2,1,3" }
+// (async () => {
+//     const res = this.solution(entrada);
+//     console.log(res)
+// })();
